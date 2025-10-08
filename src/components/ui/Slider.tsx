932042f996292c0b13552f5efd6b23f302d5fe89
Jamie <jamie7williams@gmail.com>
2025-10-08 21:42:19 +0100

    Style Slider component UI

--- src/components/ui/Slider.tsx
+++ src/components/ui/Slider.tsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+import "@/styles/slider.css";
+
 const Slider = () => {
-  return <input type="range" min="0" max="100" />;
+  // Temporary
+  const [value, setValue] = useState(50);
+
+  return (
+    <input
+      type="range"
+      min="0"
+      max="100"
+      value={value}
+      onChange={(e) => setValue(Number(e.target.value))}
+      style={{ "--value": `${value}%` } as React.CSSProperties}
+      className="slider w-full h-1.5 rounded-full cursor-pointer appearance-none bg-neutral-300"
+    />
+  );
 };
 
 export default Slider;
