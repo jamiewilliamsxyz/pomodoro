de2b84074c928a16aaf8726e6a5042cd27e42dda
Jamie <jamie7williams@gmail.com>
2025-10-11 13:40:15 +0100

    Remove capitaliseFirstLetter
    Removed capitaliseFirstLetter lib function and moved the code into the settings Content component since I am only using it once

--- src/components/features/settings/Content.tsx
+++ src/components/features/settings/Content.tsx
@@ -1,6 +1,5 @@
 import { renderSectionOptions } from "@/lib/settings/renderSectionOptions";
 import { useSettings } from "@/context/settingsContext";
-import { capitaliseFirstLetter } from "@/lib/capitaliseFirstLetter";
 
 const Content = () => {
   const { activeSection } = useSettings();
@@ -8,7 +7,8 @@ const Content = () => {
   return (
     <div className="p-4 flex flex-col gap-3">
       <h5 className="font-medium text-lg text-neutral-800">
-        {capitaliseFirstLetter(activeSection)} Settings
+        {activeSection.charAt(0).toUpperCase() + activeSection.slice(1) + " "}
+        Settings
       </h5>
       <div className="flex flex-col gap-4">
         {renderSectionOptions({ activeSection })}
