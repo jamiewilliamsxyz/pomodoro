509021c20143ec760d15f0593ce00b314fbe1165
Jamie <jamie7williams@gmail.com>
2025-10-12 17:26:58 +0100

    Add QuoteDisplay to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Timer from "@/components/features/timer/Timer";
+import QuoteDisplay from "@/components/features/quotes/QuoteDisplay";
 
 const Home = () => {
   return (
     <div className="flex items-center justify-center h-screen">
       <Timer />
+      <QuoteDisplay />
     </div>
   );
 };
