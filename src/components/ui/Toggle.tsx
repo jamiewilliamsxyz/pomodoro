3e70eadee56fc65613d7dcde29ad60ba7a3af881
Jamie <jamie7williams@gmail.com>
2025-10-12 17:04:50 +0100

    Ajust Toggle style

--- src/components/ui/Toggle.tsx
+++ src/components/ui/Toggle.tsx
@@ -16,7 +16,7 @@ const Toggle = ({ id, value }: { id: string; value: boolean }) => {
   return (
     <label
       htmlFor={id}
-      className="relative inline-flex items-center cursor-pointer mt-2"
+      className="relative inline-flex items-center cursor-pointer mt-2 max-w-fit"
     >
       <input
         type="checkbox"
@@ -27,7 +27,7 @@ const Toggle = ({ id, value }: { id: string; value: boolean }) => {
           handleChange(e.target.checked);
         }}
       />
-      <div className="group peer bg-white rounded-full duration-200 w-12 h-6 ring-[1.5px] ring-neutral-300 after:duration-200 after:bg-neutral-300 peer-checked:after:bg-emerald-300 peer-checked:ring-emerald-300 after:rounded-full after:absolute after:h-5 after:w-5 after:top-0.5 after:left-1 after:flex after:justify-center after:items-center peer-checked:after:translate-x-5.5"></div>
+      <div className="group peer bg-white rounded-full duration-200 w-12 h-6 ring-[1.5px] ring-neutral-300 after:duration-200 after:bg-neutral-300 peer-checked:after:bg-emerald-300 after:rounded-full after:absolute after:h-5 after:w-5 after:top-0.5 after:left-1 after:flex after:justify-center after:items-center peer-checked:after:translate-x-5.5"></div>
     </label>
   );
 };
