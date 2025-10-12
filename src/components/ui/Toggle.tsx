b86b096e1953610d19bc0ce628d4b3f1052c9e90
Jamie <jamie7williams@gmail.com>
2025-10-12 13:53:12 +0100

    Change Toggle component label to div

--- src/components/ui/Toggle.tsx
+++ src/components/ui/Toggle.tsx
@@ -1,9 +1,9 @@
 const Toggle = () => {
   return (
-    <label className="relative inline-flex items-center cursor-pointer">
+    <div className="relative inline-flex items-center cursor-pointer">
       <input type="checkbox" className="sr-only peer" />
       <div className="group peer bg-white rounded-full duration-200 w-12 h-6 ring-[1.5px] ring-neutral-300 after:duration-200 after:bg-neutral-300 peer-checked:after:bg-emerald-300 peer-checked:ring-emerald-300 after:rounded-full after:absolute after:h-5 after:w-5 after:top-0.5 after:left-1 after:flex after:justify-center after:items-center peer-checked:after:translate-x-5.5"></div>
-    </label>
+    </div>
   );
 };
 
