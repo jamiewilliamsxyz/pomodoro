f2ec42e5fd4f6de8109e848b505f1e8ff8ef70d8
Jamie <jamie7williams@gmail.com>
2025-10-13 11:39:28 +0100

    Ajust padding of settings Topbar and Content on larger screens

--- src/components/features/settings/Content.tsx
+++ src/components/features/settings/Content.tsx
@@ -5,7 +5,7 @@ const Content = () => {
   const { activeSection } = useSettings();
 
   return (
-    <div className="p-4 flex flex-col gap-3">
+    <div className="p-4 sm:p-6 flex flex-col gap-3">
       <h5 className="font-medium text-lg text-neutral-800">
         {activeSection.charAt(0).toUpperCase() + activeSection.slice(1) + " "}
         Settings
--- src/components/features/settings/Topbar.tsx
+++ src/components/features/settings/Topbar.tsx
@@ -22,16 +22,16 @@ const sections: { id: ActiveSectionState; icon: JSX.Element }[] = [
 const Topbar = ({ toggleSettings }: { toggleSettings: () => void }) => {
   return (
     <nav className="flex">
-      <div className="flex py-3 px-4">
+      <div className="flex py-4 px-4 sm:px-6">
         <button
           onClick={toggleSettings}
           className="text-red-400 hover:text-red-300 transition-colors duration-200 cursor-pointer"
         >
-          <X width={26} height={26} strokeWidth={1.5} />
+          <X width={28} height={28} strokeWidth={1.5} />
         </button>
       </div>
 
-      <ul className="flex justify-evenly bg-[#ececec] rounded-bl-xl pt-3 pb-[7px] w-full">
+      <ul className="flex justify-evenly items-center bg-[#ececec] rounded-bl-xl pt-3 pb-[7px] w-full">
         {sections.map((section) => (
           <NavItem
             key={section.id}
