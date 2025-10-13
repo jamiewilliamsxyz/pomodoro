a4442ab1f4c5d1b3667548e1b025acafb4216d73
Jamie <jamie7williams@gmail.com>
2025-10-13 11:10:40 +0100

    Add max width to topbar

--- src/components/layout/Topbar.tsx
+++ src/components/layout/Topbar.tsx
@@ -3,19 +3,23 @@ import IconButton from "../ui/IconButton";
 
 const Topbar = ({ toggleSettings }: { toggleSettings: () => void }) => {
   return (
-    <nav className="flex justify-between py-5 px-5">
-      <div className="flex gap-5">
+    <nav className="p-5">
+      <div className="max-w-[95rem] mx-auto flex justify-between items-center">
+        <div className="flex gap-5">
+          <IconButton
+            icon={
+              <PictureInPicture2 width={28} height={28} strokeWidth={1.25} />
+            }
+          />
+          <IconButton
+            icon={<EyeClosed width={28} height={28} strokeWidth={1.25} />}
+          />
+        </div>
         <IconButton
-          icon={<PictureInPicture2 width={28} height={28} strokeWidth={1.25} />}
-        />
-        <IconButton
-          icon={<EyeClosed width={28} height={28} strokeWidth={1.25} />}
+          onClick={toggleSettings}
+          icon={<Settings width={28} height={28} strokeWidth={1.25} />}
         />
       </div>
-      <IconButton
-        onClick={toggleSettings}
-        icon={<Settings width={28} height={28} strokeWidth={1.25} />}
-      />
     </nav>
   );
 };
