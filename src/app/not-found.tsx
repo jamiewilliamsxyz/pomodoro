bbb5e870c4f29b1c4c0a26fa6d1786b9146d2db9
Jamie <jamie7williams@gmail.com>
2025-10-13 12:01:10 +0100

    Style not-found page; add nav link to not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,16 @@
+import Link from "next/link";
+
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+    <div className="flex flex-col items-center justify-center h-screen text-neutral-800 gap-6">
+      <h1 className="text-3xl">404 - Page Not Found</h1>
+      <p className="text-sm">The page you are looking for does not exist</p>
+      <Link
+        href={"/"}
+        className="text-sm bg-emerald-300 px-4 py-2 rounded-full hover:bg-emerald-200 hover:text-neutral-700 transition-colors duration-200"
+      >
+        Navigate to app
+      </Link>
     </div>
   );
 };
