094532f75c60edbd31f098b77f243b980e0a0965
Jamie <jamie7williams@gmail.com>
2025-10-13 11:13:15 +0100

    Add max width to Footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,8 +3,10 @@ import IconButton from "../ui/IconButton";
 
 const Footer = () => {
   return (
-    <footer className="flex justify-end p-5">
-      <IconButton icon={<Info strokeWidth={1.25} width={28} height={28} />} />
+    <footer className="p-5">
+      <div className="max-w-[95rem] mx-auto flex justify-end">
+        <IconButton icon={<Info strokeWidth={1.25} width={28} height={28} />} />
+      </div>
     </footer>
   );
 };
