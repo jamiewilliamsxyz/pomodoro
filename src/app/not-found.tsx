ccbee44803dac6aec6c0aa4aa346148b9a82379c
Jamie <jamie7williams@gmail.com>
2025-10-13 17:19:07 +0100

    Remove height property from the className

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const NotFoundPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-neutral-800 gap-6">
+    <div className="flex flex-col items-center justify-center text-neutral-800 gap-6">
       <h1 className="text-3xl">404 - Page Not Found</h1>
       <p className="text-sm">The page you are looking for does not exist</p>
       <Link
