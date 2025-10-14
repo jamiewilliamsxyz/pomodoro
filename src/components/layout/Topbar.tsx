d0caa0ed952dc5475e4b335925cb6f66b7abeb1a
Jamie <jamie7williams@gmail.com>
2025-10-14 13:06:16 +0100

    Pass toggle functions into mode buttons; hide PiP button if no browser support

--- src/components/layout/Topbar.tsx
+++ src/components/layout/Topbar.tsx
@@ -1,18 +1,28 @@
+"use client";
+
 import { EyeClosed, PictureInPicture2, Settings } from "lucide-react";
+import { useModes } from "@/context/modesContext";
 import IconButton from "../ui/IconButton";
 import OpenModalButton from "../ui/OpenModalButton";
 
 const Topbar = () => {
+  const { isPiPSupported, toggleIsPiPMode, toggleIsFocusMode } = useModes();
+
   return (
     <nav className="p-5">
       <div className="max-w-[95rem] mx-auto flex justify-between items-center">
         <div className="flex gap-5">
+          {isPiPSupported && (
+            <IconButton
+              onClick={toggleIsPiPMode}
+              icon={
+                <PictureInPicture2 width={28} height={28} strokeWidth={1.25} />
+              }
+            />
+          )}
+
           <IconButton
-            icon={
-              <PictureInPicture2 width={28} height={28} strokeWidth={1.25} />
-            }
-          />
-          <IconButton
+            onClick={toggleIsFocusMode}
             icon={<EyeClosed width={28} height={28} strokeWidth={1.25} />}
           />
         </div>
