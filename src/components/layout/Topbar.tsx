487837621bbb7ba5f8d659eb460aa4709d1c0c5c
Jamie <jamie7williams@gmail.com>
2025-10-14 15:23:57 +0100

    Hide Topbar buttons on isFocusMode

--- src/components/layout/Topbar.tsx
+++ src/components/layout/Topbar.tsx
@@ -1,36 +1,49 @@
 "use client";
 
-import { EyeClosed, PictureInPicture2, Settings } from "lucide-react";
+import { EyeClosed, Eye, PictureInPicture2, Settings } from "lucide-react";
 import { useModes } from "@/context/modesContext";
 import IconButton from "../ui/IconButton";
 import OpenModalButton from "../ui/OpenModalButton";
 
 const Topbar = () => {
-  const { isPiPSupported, toggleIsPiPMode, toggleIsFocusMode } = useModes();
+  const { isPiPSupported, isFocusMode, toggleIsPiPMode, toggleIsFocusMode } =
+    useModes();
 
   return (
     <nav className="p-5">
-      <div className="max-w-[95rem] mx-auto flex justify-between items-center">
-        <div className="flex gap-5">
-          {isPiPSupported && (
-            <IconButton
-              onClick={toggleIsPiPMode}
-              icon={
-                <PictureInPicture2 width={28} height={28} strokeWidth={1.25} />
-              }
-            />
-          )}
-
+      {isFocusMode ? (
+        <div className="max-w-[95rem] mx-auto flex justify-between items-center">
           <IconButton
             onClick={toggleIsFocusMode}
-            icon={<EyeClosed width={28} height={28} strokeWidth={1.25} />}
+            icon={<Eye width={28} height={28} strokeWidth={1.25} />}
           />
         </div>
-        <OpenModalButton
-          modal="settings"
-          icon={<Settings width={28} height={28} strokeWidth={1.25} />}
-        />
-      </div>
+      ) : (
+        <div className="max-w-[95rem] mx-auto flex justify-between items-center">
+          <div className="flex gap-5">
+            <IconButton
+              onClick={toggleIsFocusMode}
+              icon={<EyeClosed width={28} height={28} strokeWidth={1.25} />}
+            />
+            {isPiPSupported && (
+              <IconButton
+                onClick={toggleIsPiPMode}
+                icon={
+                  <PictureInPicture2
+                    width={28}
+                    height={28}
+                    strokeWidth={1.25}
+                  />
+                }
+              />
+            )}
+          </div>
+          <OpenModalButton
+            modal="settings"
+            icon={<Settings width={28} height={28} strokeWidth={1.25} />}
+          />
+        </div>
+      )}
     </nav>
   );
 };
