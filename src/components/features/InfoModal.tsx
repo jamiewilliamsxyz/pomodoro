5a7864546ceec0409c5c5164e602b9e9ddd2e7a5
Jamie <jamie7williams@gmail.com>
2025-10-15 13:21:03 +0100

    Ajust InfoModal style

--- src/components/features/InfoModal.tsx
+++ src/components/features/InfoModal.tsx
@@ -7,26 +7,28 @@ const InfoModal = () => {
       onClick={(e) => {
         e.stopPropagation();
       }}
-      className="p-3 sm:px-5 sm:py-4 flex flex-col gap-4.5 relative top-[8vh] bg-neutral-50 max-w-sm h-fit rounded-xl shadow-sm"
+      className="px-6"
     >
-      <CloseModalButton />
-      <div className="ml-1 flex flex-col gap-3.5">
-        <TextLink
-          url="https://github.com/jamiewilliamsxyz"
-          text="My GitHub Profile"
-        />
-        <TextLink
-          url="https://github.com/jamiewilliamsxyz/pomodoro"
-          text="Project Source Code"
-        />
-        <i>
-          This project uses the QuoteSlate API, a free and open-source quote
-          API. Check it out {""}
+      <div className="p-3 sm:px-5 sm:py-4 flex flex-col gap-4.5 relative top-[8vh] bg-neutral-50 max-w-sm h-fit rounded-xl shadow-sm">
+        <CloseModalButton />
+        <div className="ml-1 flex flex-col gap-3.5">
           <TextLink
-            url="https://github.com/Musheer360/QuoteSlate"
-            text="here"
+            url="https://github.com/jamiewilliamsxyz"
+            text="My GitHub Profile"
           />
-        </i>
+          <TextLink
+            url="https://github.com/jamiewilliamsxyz/pomodoro"
+            text="Project Source Code"
+          />
+          <i>
+            This project uses the QuoteSlate API, a free and open-source quote
+            API. Check it out {""}
+            <TextLink
+              url="https://github.com/Musheer360/QuoteSlate"
+              text="here"
+            />
+          </i>
+        </div>
       </div>
     </div>
   );
