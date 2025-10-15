e459e850a7e14cfa785e7da700c057a26cd1db31
Jamie <jamie7williams@gmail.com>
2025-10-15 13:03:55 +0100

    Refactor Topbar to be responsive on mobile

--- src/components/layout/Topbar.tsx
+++ src/components/layout/Topbar.tsx
@@ -6,28 +6,30 @@ import OpenModalButton from "../ui/OpenModalButton";
 
 const Topbar = () => {
   return (
-    <nav className="mt-6 w-[34rem] px-5 py-3 mx-auto flex justify-between rounded-full shadow-sm">
-      <div className="flex gap-3 items-center">
-        <Link href="/">
-          <Image src={logo.src} width={26} height={26} alt="Pomodoro logo" />
-        </Link>
+    <div className="px-6 pt-6 pb-1">
+      <nav className="w-full max-w-[33.5rem] mx-auto px-5 py-3 flex justify-between rounded-full shadow-sm">
+        <div className="flex gap-3 items-center">
+          <Link href="/">
+            <Image src={logo.src} width={26} height={26} alt="Pomodoro logo" />
+          </Link>
 
-        <h1 className="text-lg font-medium text-neutral-800 tracking-normal">
-          Pomodoro
-        </h1>
-      </div>
+          <h1 className="text-lg font-medium text-neutral-800 tracking-normal">
+            Pomodoro
+          </h1>
+        </div>
 
-      <div className="flex gap-5 items-center">
-        <OpenModalButton
-          modal="settings"
-          icon={<Settings width={26} height={26} strokeWidth={1.25} />}
-        />
-        <OpenModalButton
-          modal="info"
-          icon={<Info width={26} height={26} strokeWidth={1.25} />}
-        />
-      </div>
-    </nav>
+        <div className="flex gap-5 items-center">
+          <OpenModalButton
+            modal="settings"
+            icon={<Settings width={26} height={26} strokeWidth={1.25} />}
+          />
+          <OpenModalButton
+            modal="info"
+            icon={<Info width={26} height={26} strokeWidth={1.25} />}
+          />
+        </div>
+      </nav>
+    </div>
   );
 };
 
