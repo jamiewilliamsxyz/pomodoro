76aa35bde29bb72cd62e8717ea1cc9b4cb19a427
Jamie <jamie7williams@gmail.com>
2025-10-16 12:50:28 +0100

    Remove useToggle hook

--- src/hooks/useToggle.tsx
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-export const useToggle = (initialValue: boolean): [boolean, () => void] => {
-  const [on, setOn] = useState(initialValue);
-  const toggle = () => setOn((prev) => !prev);
-  return [on, toggle];
-};
