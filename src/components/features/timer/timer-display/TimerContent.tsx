dac7c0494bfd279c8900f5f3debb91a26cf9e316
Jamie <jamie7williams@gmail.com>
2025-10-17 12:54:28 +0100

    Change countdown timer text colour

--- src/components/features/timer/timer-display/TimerContent.tsx
+++ src/components/features/timer/timer-display/TimerContent.tsx
@@ -18,7 +18,7 @@ const TimerContent = ({
   return (
     <div className="absolute flex flex-col items-center gap-5">
       <h2 className="text-xl text-neutral-700">{sessionType}</h2>
-      <h3 className="text-center text-6xl font-medium tabular-nums">
+      <h3 className="text-center text-6xl font-medium tabular-nums text-neutral-800">
         {minutes}:{seconds.toString().padStart(2, "0")}
       </h3>
       <h4 className="text-lg text-neutral-700">
