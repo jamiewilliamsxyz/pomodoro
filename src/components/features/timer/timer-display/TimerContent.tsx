f940ded519264b50e8b00ee80d3eaac55190dae0
Jamie <jamie7williams@gmail.com>
2025-10-19 20:05:08 +0100

    Add accessibility attributes
    Added aria-live/labels and the role attribute to the TimerContent headings

--- src/components/features/timer/timer-display/TimerContent.tsx
+++ src/components/features/timer/timer-display/TimerContent.tsx
@@ -17,11 +17,25 @@ const TimerContent = ({
 
   return (
     <div className="absolute flex flex-col items-center gap-5">
-      <h2 className="text-xl text-base-300">{sessionType}</h2>
-      <h3 className="text-center text-6xl font-medium tabular-nums text-base-200">
+      <h2
+        aria-live="polite"
+        aria-label={`Current session type: ${sessionType}`}
+        className="text-xl text-base-300"
+      >
+        {sessionType}
+      </h2>
+      <h3
+        role="timer"
+        aria-label={`Time left: ${minutes} minutes and ${seconds} seconds`}
+        className="text-center text-6xl font-medium tabular-nums text-base-200"
+      >
         {minutes}:{seconds.toString().padStart(2, "0")}
       </h3>
-      <h4 className="text-lg text-base-300">
+      <h4
+        aria-live="polite"
+        aria-label={`Round ${roundNumber} of ${settings.timer.roundsUntilLongBreak}`}
+        className="text-lg text-base-300"
+      >
         {roundNumber}/{settings.timer.roundsUntilLongBreak}
       </h4>
     </div>
